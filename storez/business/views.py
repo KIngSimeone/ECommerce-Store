7f8fbe6a8db5f60f0877f9ad4a1570275ddac1cf
Kingsimeone <simeoneumoh@outlook.com>
2020-11-27 10:29:55 +0100

    Create Business function created

--- storez/business/views.py
+++ storez/business/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import(
+                    Business,
+                    BusinessAddress,
+                    Product,
+                    )
+from django.utils import timezone
+from datetime import datetime, date, timedelta
+from django.conf import settings
+
+
+import secrets
+
+# import the logging library
+import logging
+
+# Create and instance of a logger
+logger = logging.getLogger(__name__)
+
+def createBusiness(user,businessName,businessEmail,phone):
+    try:
+        business = Business(
+                    user=user,
+                    businessName=businesstName,
+                    businessEmail=businessEmail,
+                    phone=phone
+                     )
+
+        business.save()
+        return business
+
+    except Exception as e:
+        logger.error("createBusiness@error")
+        logger.error(e)
+        return None
+
+def createBusinessAddress(user,business,street,city,state,country,zipCode): 
+    try:
+        address = BusinessAddress(
+                user=user,
+                business=business,
+                street=street,
+                city=city,
+                state=state,
+                country=country,
+                zipCode=zipCode
+                )
+
+        address.save()
+        return address
+    except Exception as e:
+        logger.error("CreateBusinessAddress@error")
+        logger.error(e)
+        return None

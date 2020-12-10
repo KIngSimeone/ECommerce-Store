78343c750c3736ed4ee78760102577897ca6e1e6
Kingsimeone <simeoneumoh@outlook.com>
2020-12-10 15:53:28 +0100

    s3 endpoint changed

--- storez/config/settings.py
+++ storez/config/settings.py
@@ -147,7 +147,7 @@ if  USE_S3 == 'TRUE':
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     AWS_LOCATION = 'static'

8f441ee6bd8d5341236962acfe7e2caed4312fa0
Kingsimeone <simeoneumoh@outlook.com>
2020-12-15 08:48:27 +0100

    BusinessLogo registered in admin

--- storez/business/admin.py
+++ storez/business/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from .models import (
                     Business,
                     BusinessAddress,
-                    Photo,
+                    BusinessLogo,
                     Product
 )
 admin.site.register(Business)
 admin.site.register(BusinessAddress)
-admin.site.register(Photo)
+admin.site.register(Businesslogo)
 admin.site.register(Product)
 

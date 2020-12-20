7fef1abd9244f35210c66625802c4b9a10dc7e22
Kingsimeone <simeoneumoh@outlook.com>
2020-12-20 16:02:19 +0100

    celery and settings update

--- storez/config/celery.py
+++ storez/config/celery.py
@@ -12,11 +12,11 @@ app = Celery(storez)
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+#   should have a `CELERY_` prefix
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs
+# Load task modules from all registered Django app config
 app.autodiscover_tasks()
 
 @app.task(bind=True)
--- storez/config/settings.py
+++ storez/config/settings.py
@@ -161,7 +161,7 @@ if  USE_S3 == 'TRUE':
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
 
-else:  
+else:  s
     MEDIA_URL = '/mediafiles/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
@@ -176,7 +176,7 @@ from corsheaders.defaults import default_headers
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'accessToken',
-    'secret'
+    'secret' 
 ]
 
 # Celery location to find redis
